3e15d19cc7e650e6f3b4176e08a30d9e2b15a210
ks.lee <ks.lee@ysmcorp.com>
2017-09-18 19:24:48 +0900

    [3.components] quiz

---
+++ 3_components/quiz/js/app.js
@@ -0,0 +1,30 @@
+// Global Component
+Vue.component('todo-item', {
+  template: '<p>This is a child component</p>'
+});
+
+// 실습 #1 - `todo-footer` 컴포넌트 전역 등록
+// <p>This is another child global component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
+/* 실습 1번 해답
+Vue.component('todo-footer', {
+  template: '<p>This is another child global component</p>'
+})
+*/
+
+// 'todo-footer' Local 컴포넌트
+var cmp = {
+  template : '<p>This is another child local component</p>'
+};
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message : 'This is a parent component'
+  },
+
+  // 실습 #2 - `todo-footer` 컴포넌트 지역 등록
+  // <p>This is another child local component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
+  components: {
+    'todo-footer': cmp
+  }
+});
