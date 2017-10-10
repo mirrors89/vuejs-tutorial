73fb1bc0d39fdf96d8faaa69b9f2246e96ebe166
mirrors89 <lee01494@gmail.com>
2017-10-10 20:09:12 +0900

    [5.nested_router] nested router

--- 5_nested_router/js/app.js
+++ 5_nested_router/js/app.js
@@ -2,20 +2,9 @@ var Login = {
   template: `
     <div>
       Login Section
-      <router-view>
-        <form action="/" method="post">
-          <div>
-              <label for="account">E-mail : </label>
-              <input type="email" id="account">
-          </div>
-          <div>
-              <label for="password">Password : </label>
-              <input type="password" id="password">
-          </div>
-        </form>
-      </router-view>
+      <router-view></router-view>
     </div>
-  `
+  `,
 };
 var LoginForm = {
   template: `
@@ -29,16 +18,15 @@ var LoginForm = {
           <input type="password" id="password">
       </div>
     </form>
-  `
+  `,
 };
-
 var List = {
   template: `
     <div>
       List Section
       <router-view></router-view>
     </div>
-  `
+  `,
 };
 var ListItems = {
   template: `
@@ -47,8 +35,11 @@ var ListItems = {
       <li>2</li>
       <li>3</li>
     </ul>
-  `
+  `,
 };
+
+// 할일 #2
+// Main 컴포넌트와 그 하위 컴포넌트를 아래 등록해보세요.
 
 var routes = [
   {
@@ -64,7 +55,10 @@ var routes = [
     children: [
       { path: '', component: ListItems }
     ]
-  }
+  },
+  // 할일 #1
+  // `/main` URL 에서 동작할 라우터를 하나 등록하고,
+  // 해당 라우터에서 동작할 Main 컴포넌트와 하위 컴포넌트를 생성하여 등록합니다.
 ];
 
 var router = new VueRouter({
